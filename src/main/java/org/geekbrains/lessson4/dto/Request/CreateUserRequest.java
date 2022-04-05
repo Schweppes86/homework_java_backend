73949f133d8fb989df2649d0475caf3d4222ebd0
Schweppes86 <schweppes86@gmail.com>
2022-04-06 01:51:24 +0300

    hw_lesson4

---
+++ src/main/java/org/geekbrains/lessson4/dto/Request/CreateUserRequest.java
@@ -0,0 +1,23 @@
+package org.geekbrains.lessson4.dto.Request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("firstName")
+    private String firstName;
+    @JsonProperty("lastName")
+    private String lastName;
+    @JsonProperty("email")
+    private String email;
+}
